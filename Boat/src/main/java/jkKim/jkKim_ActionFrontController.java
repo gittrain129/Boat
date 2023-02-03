973cda5553f712d76723d1de9b2a90133152451b
fiatl <fiatl@39.118.36.21>
2023-02-03 17:29:56 +0900

    frontcontroller 작업

--- Boat/src/main/java/jkKim/jkKim_ActionFrontController.java
+++ Boat/src/main/java/jkKim/jkKim_ActionFrontController.java
@@ -1,5 +1,78 @@
 package jkKim;
 
-public class jkKim_ActionFrontController {
+import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("*.jk")
+public class jkKim_ActionFrontController extends javax.servlet.http.HttpServlet {
+	private static final long serialVersionUID = 871322249870799836L;
+	
+
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		String RequestURI = request.getRequestURI();
+		System.out.println("RequestURI = " + RequestURI);
+		
+		String contextPath = request.getContextPath(); // "/JspProject"
+		System.out.println("contextPath = " + contextPath);
+
+		String command = RequestURI.substring(contextPath.length()); // "/login.net"
+		System.out.println("command = " + command);
+		
+		jkKim_ActionForward forward = null;
+		jkKim_Action action = null;
+
+		switch (command) {
+		case "/address.jk":
+			action = new AddressViewAction();
+			break;
+			
+		}// switch end
+		
+		
+		
+		
+		forward = action.execute(request, response);
+
+		if (forward != null) {
+			if (forward.isRedirect()) {// 리다이렉트 됩니다.
+				response.sendRedirect(forward.getPath());
+			} else { // 포워딩됩니다
+				RequestDispatcher dispatcher = request.getRequestDispatcher(forward.getPath());
+				dispatcher.forward(request, response);
+			}
+		}
+
+	}// doProcess end
+
+	// doProcess메서드를 구현하여 get방식이든 post방식으로 전송되어 오든 같은 메서드에서 요청을 처리할수있도록함
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		request.setCharacterEncoding("utf-8");
+		doProcess(request, response);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
 }
---
+++ Boat/src/main/java/jkKim/member/db/jkKim_MemberDAO.java
@@ -0,0 +1,33 @@
+package jkKim.member.db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class jkKim_MemberDAO {
+	
+	private DataSource ds;
+
+	// 생ㅅ어자에서 JNDI리소스를 참조하여 Connection객체를 얻어옵니다
+	public jkKim_MemberDAO() {
+		try {
+			Context init = new InitialContext();
+			ds = (DataSource) init.lookup("java:comp/env/jdbc/OracleDB");
+		} catch (Exception ex) {
+			System.out.println("db연결 실패: " + ex);
+		}
+	} //생성자end
+	
+	public int addressview() {
+		return 0;
+	}
+	
+	
+	
+	
+}
