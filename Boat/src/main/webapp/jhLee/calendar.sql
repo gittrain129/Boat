0851cead3db595e45c07fd0788a9cdd8783afdb2
izzy215 <jihyeon3@gmail.com>
2023-02-06 12:17:34 +0900

    230206start

--- Boat/src/main/webapp/jhLee/calendar.sql
+++ Boat/src/main/webapp/jhLee/calendar.sql
@@ -8,6 +8,26 @@
 	end_date varchar2(10),
 	allday varchar2(4) check (allday in ('y','n')));
 	
+	drop table boat_Calendar cascade constraints purge;
+
+--dept empno추가 필요
+create table boat_Calendar (
+schedule_code number (2) primary key,
+event_name varchar2 (55),
+start_date varchar2  (55),
+end_date varchar2	 (55),
+allday varchar2(10) check (allday  in('true','false'))
+);
+delete from boat_Calendar;
+
+select * from boat_Calendar;
+
+create sequence cal_seq nocache;
+
+insert into boat_Calendar 
+	values(cal_seq.nextval,?,?,?,?);
+
+	drop sequence cal_seq;
 	
 	
 create sequence cal_seq nocache;
