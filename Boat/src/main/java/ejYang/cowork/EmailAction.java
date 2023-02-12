e5a37a2afaa956b5e2db6394f753cb10c8fb21ac
fiatl <fiatl@39.118.36.21>
2023-02-12 12:48:36 +0900

    이메일액션.java

--- Boat/src/main/java/ejYang/cowork/EmailAction.java
+++ Boat/src/main/java/ejYang/cowork/EmailAction.java
@@ -18,10 +18,10 @@ public class EmailAction implements Action{
 //		HttpSession session = request.getSession();
 //		String adminempno = (String) session.getAttribute("empno");//admin
 		
-//		String memberempno = request.getParameter("empno");//주소록에서 넘겨받을 회원
+		String memberempno = request.getParameter("empno");//주소록에서 넘겨받을 회원
 		String adminempno = "ADMIN";//페이지 완성되면 삭제
-		String memberempno = "ADMIN3";//페이지 완성되면 삭제
-		
+		//String memberempno = "ADMIN3";//페이지 완성되면 삭제
+		System.out.println("전송받은 memberempno = " + memberempno);
 		ActionForward forward=new ActionForward();
 		MemberDAO mdao = new MemberDAO();
 		Member admininfo = mdao.member_info(adminempno);
